fc252cf7811522f4024c38be5c456e5caf2b264c
Brendan O'Connor <bhoconnor@gmail.com>
2023-06-18 17:30:38 -0400

    Small update to 2-globals.js comment

--- 01-node-tutorial/answers/2-globals.js
+++ 01-node-tutorial/answers/2-globals.js
@@ -1,4 +1,4 @@
 console.log(__dirname);
 console.log(process.env.MY_VAR);
 
-// Note: I ran export MY_VAR="Hi there!" in the terminal and then ran this file, and it returned the correct directory name for the 1st console.log above and "Hi there!" for the 2nd.
+// KEY: I had to run export MY_VAR="Hi there!" in the terminal and then ran this file, and it returned the correct directory name for the 1st console.log above and "Hi there!" for the 2nd.
