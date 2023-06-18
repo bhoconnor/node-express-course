bcde31e9293dadf87fab57199573145559ba334c
Brendan O'Connor <bhoconnor@gmail.com>
2023-06-18 17:32:37 -0400

    Fix spelling typo in 7-mind-grenade function

--- 01-node-tutorial/answers/7-mind-grenade.js
+++ 01-node-tutorial/answers/7-mind-grenade.js
@@ -4,7 +4,7 @@ const horse3 = 25;
 
 function horsesTotalAge() {
   console.log(
-    `The total age of the 3 hor ses adds up to ${horse1 + horse2 + horse3}!!`
+    `The total age of the 3 horses adds up to ${horse1 + horse2 + horse3}!!`
   );
 }
 
