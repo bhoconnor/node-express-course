2a1e17430c5594c1fd27bb361892bfba46942e72
Brendan O'Connor <bhoconnor@gmail.com>
2023-06-19 07:05:49 -0400

    Add comments

--- 01-node-tutorial/answers/8-os.js
+++ 01-node-tutorial/answers/8-os.js
@@ -6,7 +6,9 @@ console.log(user);
 
 // Info about computer
 const compInfo = {
+  // Operating system name (eg, Linux for Linux, Darwin for Mac, Windows_NT for Windows)
   name: os.type(),
+  // Operating system "kernel version" (also found under About this Mac > System Information)
   release: os.release(),
   totalMem: os.totalmem(),
   freeMem: os.freemem(),
