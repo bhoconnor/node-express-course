d237cf9bd1e1a133794090775536a161b122f525
Brendan O'Connor <bhoconnor@gmail.com>
2023-06-19 07:44:09 -0400

    Add comment & slight string edit

--- 01-node-tutorial/answers/10-fs-sync.js
+++ 01-node-tutorial/answers/10-fs-sync.js
@@ -1,12 +1,12 @@
 // Destructured way of doing fs.readFileSync & fs.writeFileSync
-
+// Interestingly uses require with a module/file that is NOT written by us in this assignment, which is the main way it was done with all the other uses of require
 const { readFileSync, writeFileSync, appendFileSync } = require("fs");
 
 writeFileSync("./temporary/fileA.txt", "This is line 1. ", { flag: "a" });
 
 appendFileSync("./temporary/fileA.txt", "This is line 2. ", { flag: "a" });
 
-appendFileSync("./temporary/fileA.txt", "This is line 3.");
+appendFileSync("./temporary/fileA.txt", "This is line 3. ");
 
 const readWriteFile = readFileSync("./temporary/fileA.txt", "utf8");
 
